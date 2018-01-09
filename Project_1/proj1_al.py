45c49795611ab15d4329bd8c41b8c272120c39cc
Adam Lee <AdamBLee@users.noreply.github.com>
2018-01-08 20:25:14 -0700

    added inital code for project 1

---
+++ Project_1/proj1_al.py
@@ -0,0 +1,37 @@
+TIP_EXCELLENT = .20
+TIP_GOOD = .15
+TIP_POOR = .10
+
+def calculate_tip(tip_percentage, total):
+	tip = total / tip_percentage
+	
+	return tip
+	
+def 
+	
+def get_string_output(total, tip):
+	
+
+	
+	pass
+	
+	
+	
+def main():
+	
+	#print
+	input = input('Enter the cost of the meal: ')
+	
+	input.split('')
+	
+	input_amount = input[1] 
+	
+	tip_excellent = calculate_tip(TIP_EXCELLENT, total)
+	
+	tip_good = calculate_tip(TIP_GOOD, total)
+
+	tip_poor = calculate_tip(TIP_POOR, total)					
+
+	pass	
+			
+main()
