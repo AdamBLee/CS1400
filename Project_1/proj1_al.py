80c45809db1c2ce6ae7b1ce369a0be5f362f7a86
Adam Lee <AdamBLee@users.noreply.github.com>
2018-01-08 21:30:40 -0700

    coding complete, output correctly formatted

--- Project_1/proj1_al.py
+++ Project_1/proj1_al.py
@@ -1,37 +1,50 @@
+#Comment at top of file includes proper identifying information,
+#a description of the problem, and a description of the important
+#points of your solution
+
+from decimal import Decimal
+
 TIP_EXCELLENT = .20
-TIP_GOOD = .15
+TIP_AVERAGE = .15
 TIP_POOR = .10
 
-def calculate_tip(tip_percentage, total):
-	tip = total / tip_percentage
+def calculate_tip(tip_percentage, meal_cost):
+	
+	tip = meal_cost * tip_percentage
 	
 	return tip
 	
-def 
+def calculate_total(tip, meal_cost):
 	
-def get_string_output(total, tip):
+	return round(Decimal(meal_cost + Decimal(tip)),2)
 	
-
+def get_decimal_format(number_to_format):
 	
-	pass
-	
-	
+		return round(Decimal(number_to_format), 2)	
 	
 def main():
 	
-	#print
-	input = input('Enter the cost of the meal: ')
+	user_input = input('Enter the cost of the meal: ')
 	
-	input.split('')
-	
-	input_amount = input[1] 
-	
-	tip_excellent = calculate_tip(TIP_EXCELLENT, total)
-	
-	tip_good = calculate_tip(TIP_GOOD, total)
+	user_input_list = user_input.split(' ')
 
-	tip_poor = calculate_tip(TIP_POOR, total)					
+	meal_cost = float(user_input_list[1])
+	
+	tip_excellent_calculated = calculate_tip(TIP_EXCELLENT, meal_cost)
+	
+	tip_average_calculated = calculate_tip(TIP_AVERAGE, meal_cost)
 
-	pass	
+	tip_poor_calculated = calculate_tip(TIP_POOR, meal_cost)					
+	
+	meal_cost = round(Decimal(meal_cost), 2)
+
+	print('Cost of meal: ${0}'.format(meal_cost))
+	print('Excellent Service tip: ${0} total: ${1}'.format(get_decimal_format(tip_excellent_calculated),calculate_total(tip_excellent_calculated,meal_cost)))
+	print('average Service tip: ${0} total: ${1}'.format(get_decimal_format(tip_average_calculated),calculate_total(tip_average_calculated,meal_cost)))
+	print('Poor Service tip: ${0} total: ${1}'.format(get_decimal_format(tip_poor_calculated),calculate_total(tip_poor_calculated,meal_cost)))
+	
+#	print(output)
 			
 main()
+
+#What I learned and what difficulties
